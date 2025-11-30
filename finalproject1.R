e40ec4103672a080b2bcae6d2e80d6623ab72b0b
FayeDing99 <arthurding0122@gmail.com>
2025-11-30 15:47:50 -0500

    secpond part updated

--- finalproject1.R
+++ finalproject1.R
@@ -11,7 +11,10 @@ library(packrat)
 
 install.packages("haven")   
 library(haven)
-ajr_dta <- read_dta("~/Desktop/finalproject/maketable3.dta")
+install.packages("fixest")
+library(fixest)
+install.packages("modelsummary")
+library(modelsummary)
 
 
 
@@ -40,6 +43,39 @@ panel_A_models <- list(
   "(9)" = feols(avexpr ~ logem4, data = lpgp_sample), # Uses smaller sample
   "(10)" = feols(avexpr ~ logem4 + lat_abst, data = lpgp_sample) # Uses smaller sample
 )
+
+# --- Generate Table for Panel A ---
+modelsummary(
+  panel_A_models,
+  output = "gt",
+  title = "Table 3, Panel A: Determinants of Institutions",
+  coef_map = c("cons00a" = "Constraint on Executive in 1900", "democ00a" = "Democracy in 1900",
+               "cons1" = "Constraint on Executive at Independence", "indtime" = "Date of Independence",
+               "euro1900" = "European Settlements in 1900", "logem4" = "Log Settler Mortality",
+               "lat_abst" = "Distance from Equator"),
+  gof_map = list(list("raw" = "nobs", "clean" = "Num. Obs.", "fmt" = 0),
+                 list("raw" = "r.squared", "clean" = "R-squared", "fmt" = 3)),
+  stars = TRUE,
+  notes = "Notes: Dependent Variable is Average Expropriation Risk, 1985-95. Standard errors in parentheses."
+)
+
+# --- Run Regressions for Panel B ---
+panel_B_models <- list(
+  # Dep Var: Constraint on Executive in 1900
+  "(1)" = feols(cons00a ~ euro1900, data = lpgp_sample),
+  "(2)" = feols(cons00a ~ euro1900 + lat_abst, data = lpgp_sample),
+  "(3)" = feols(cons00a ~ logem4, data = main_sample),
+  "(4)" = feols(cons00a ~ logem4 + lat_abst, data = main_sample),
+  # Dep Var: Democracy in 1900
+  "(5)" = feols(democ00a ~ euro1900, data = lpgp_sample),
+  "(6)" = feols(democ00a ~ euro1900 + lat_abst, data = lpgp_sample),
+  "(7)" = feols(democ00a ~ logem4, data = lpgp_sample),
+  "(8)" = feols(democ00a ~ logem4 + lat_abst, data = lpgp_sample),
+  # Dep Var: European Settlements in 1900
+  "(9)" = feols(euro1900 ~ logem4, data = lpgp_sample),
+  "(10)" = feols(euro1900 ~ logem4 + lat_abst, data = lpgp_sample)
+)
+
 
 
 
